ad4078be4aa23e0a25b6285f9eb357fa33ef6279
Onur ALACA <onuralcaa@gmail.com>
2024-02-19 23:55:47 +0300

    Pointers 2

---
+++ Pointers_2_BTK.cpp
@@ -0,0 +1,23 @@
+//BTK akademi C programlama eðitimi 19.02.2023
+//Onur ALACA
+
+//Ýþaretçiler...
+
+#include <stdio.h>
+
+int main()
+{
+	int sayi = 10;
+	int * isaretci;
+	
+	isaretci = &sayi; //pointer atama
+	
+	printf("sayi deðiskeninin adresi : %d\n", &sayi);
+	printf("sayi deðiskeninin icerigi : %d\n", sayi);
+	
+	printf("isaretci deðiskeninin adresi : %d\n", &isaretci);
+	printf("isaretci deðiskeninin icerigi : %d\n", isaretci);
+	printf("isaretci deðiskeninin isaret ettigi deger : %d\n", *isaretci);
+
+	return 0;
+}
