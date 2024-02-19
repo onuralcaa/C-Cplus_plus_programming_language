41072680498ce966fcc7fc58439f861db1af0818
Onur ALACA <onuralcaa@gmail.com>
2024-02-20 00:01:00 +0300

    Pointers 3

---
+++ Pointers_3_BTK.cpp
@@ -0,0 +1,24 @@
+//BTK akademi C programlama eðitimi 19.02.2023
+//Onur ALACA
+
+//Ýþaretçiler...
+
+#include <stdio.h>
+
+int main()
+{
+	int sayi1, sayi2, toplam;
+	int *isaretci1, *isaretci2;
+	
+	isaretci1 = &sayi1;
+	isaretci2 = &sayi2;
+	
+	printf("Iki sayi giriniz :");
+	scanf("%d%d", isaretci1, isaretci2);
+	
+	toplam = *isaretci1 + *isaretci2;
+	
+	printf("Toplam : %d", toplam);
+	
+	return 0;
+}
